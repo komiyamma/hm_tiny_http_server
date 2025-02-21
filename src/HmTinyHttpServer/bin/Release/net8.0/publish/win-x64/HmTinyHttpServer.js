2b16dad9510fc110dee2b1e0659da14d3e138f8b
komiyamma <komiyama0629@gmail.com>
2025-02-21 17:27:01 +0900

    ...

---
+++ src/HmTinyHttpServer/bin/Release/net8.0/publish/win-x64/HmTinyHttpServer.js
@@ -0,0 +1,66 @@
+
+(function () {
+
+    var currentJsFileName = currentjsfilename();
+    var splitted = currentJsFileName.split("\\");
+    splitted.pop();
+    var currentJsDirectory = splitted.join("\\");
+
+    function output(msg) {
+        var dllobj = hidemaru.loadDll("HmOutputPane.dll");
+        return dllobj.dllFuncW.OutputW(hidemaru.getCurrentWindowHandle(), msg + "\r\n");
+    }
+
+    createTinyHttpServer = function (props) {
+
+        if (!props) {
+            output("引数がありません");
+            return null;
+        }
+
+        if (!props.rootFolder) {
+            output("rootFolderが指定されていません");
+            return null;
+        }
+
+        var is_directory = existfile(props.rootFolder, 1) & 0x00000010;
+        if (!is_directory) {
+            output("「" + props.rootFolder + "」というフォルダ存在しません。");
+            return null;
+        }
+
+        return {
+
+            processInfo: null,
+
+            start: function () {
+                // コマンドライン構築
+                var command = sprintf('%s\\%s "%s" %d', currentJsDirectory, "\\HmTinyHttpServer.exe", props.rootFolder, hidemaru.getCurrentWindowHandle());
+
+                // 実行してみる
+                this.processInfo = hidemaru.runProcess(command, ".", "stdioAlive", "utf8");
+                if (this.processInfo) {
+
+                    // ポート番号が出てくるので待つ。0.1～0.2秒程度なので、以下のように同期的に待っても影響は少ないいが、不安なら非同期のonReadLineの書き方でもよい。
+                    var output = this.processInfo.stdOut.readLine(1000);
+
+                    var port = Number(output);
+                    if (port > 0) {
+                        return port;
+                    }
+                }
+
+                return 0;
+            },
+
+            close: function () {
+                // 前回この「currentmacrofilename」でのjs実行空間内でサーバーを立ち上げてるなら終了
+                if (this.processInfo) {
+                    this.processInfo.stdIn.writeLine("exit");
+                }
+            }
+        };
+
+    }
+
+})();
